2a87aeec158c2cbc849d50e4bdcaddbdb2a06a15
Chris Troutner <chris.troutner@gmail.com>
2019-08-03 14:25:48 -0700

    Prototyped getting price and shift data

--- src/commands/trade-info.js
+++ src/commands/trade-info.js
@@ -13,6 +13,8 @@ const appUtils = new AppUtils()
 const BB = require("bitbox-sdk").BITBOX
 const BITBOX = new BB({ restURL: "https://rest.bitcoin.com/v2/" })
 
+const axios = require("axios")
+
 // Used for debugging and error reporting.
 const util = require("util")
 util.inspect.defaultOptions = { depth: 2 }
@@ -43,7 +45,22 @@ class TradeInfo extends Command {
       if (walletInfo.network === "testnet")
         this.BITBOX = new BB({ restURL: "https://trest.bitcoin.com/v2/" })
 
-      console.log(`hello world.`)
+      const tradeInfo = await this.fetchPair()
+      console.log(`BCH price: $${tradeInfo.rate}`)
+      console.log(`Minimum trade quantity: ${tradeInfo.min} BCH`)
+      console.log(`Maximum trade quantity: ${tradeInfo.max} BCH`)
+      console.log(" ")
+
+      console.log(`Comparison Pricing:`)
+      const bbPrice = await this.getBitcoinComPrice()
+      console.log(`Bitcoin.com Price: $${bbPrice}`)
+
+      const usdBch = await this.getCoinbasePrice()
+      console.log(`Coinbase Price: $${usdBch}`)
+
+      const avg = (Number(usdBch) + Number(bbPrice)) / 2
+      console.log(`Average price: $${avg}`)
+      console.log(" ")
     } catch (err) {
       if (err.message) console.log(err.message)
       else console.log(`Error in UpdateBalances.run: `, err)
@@ -58,6 +75,56 @@ class TradeInfo extends Command {
       throw new Error(`You must specify a wallet with the -n flag.`)
 
     return true
+  }
+
+  // Get the spot price of BCH from Coinbase.
+  async getCoinbasePrice() {
+    try {
+      const body = await axios({
+        url: `https://api.coinbase.com/v2/exchange-rates?currency=BCH`,
+        method: "get",
+        responseType: "json"
+      })
+
+      const usdBch = body.data.data.rates.USD
+
+      return usdBch
+    } catch (err) {
+      console.error(`Error in trade-info.js/getCoinbasePrice()`)
+      throw err
+    }
+  }
+
+  // Gets the BCH price from the Bitcoin.com server.
+  async getBitcoinComPrice() {
+    try {
+      const bbPrice = await this.BITBOX.Price.current("usd")
+
+      const realPrice = bbPrice / 100
+
+      return realPrice
+    } catch (err) {
+      console.error(`Error in trade-info.js/getBitcoinComPrice()`)
+      throw err
+    }
+  }
+
+  // Get trade pair info from SideShift.ai
+  async fetchPair() {
+    try {
+      const body = await axios({
+        url: `https://sideshift.ai/api/pairs/bch/usdh`,
+        method: "get",
+        responseType: "json"
+      })
+
+      //console.log(`body.data: ${JSON.stringify(body.data, null, 2)}`)
+
+      return body.data
+    } catch (err) {
+      console.error(`Error in trade-info.js/getCoinbasePrice()`)
+      throw err
+    }
   }
 }
 
