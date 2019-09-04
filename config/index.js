3167ff4e1e916d96a1373b733b9d7390124365bd
Chris Troutner <chris.troutner@gmail.com>
2019-09-04 08:59:17 -0700

    making rest.bitcoin.com the default config setting for RESTAPI

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@
 "use strict"
 
 // By default choose a local rest API.
-let RESTAPI = "bitcoin.com"
+let RESTAPI = "rest.bitcoin.com"
 
 // Override the RESTAPI setting if envronment variable is set.
 if (process.env.RESTAPI) RESTAPI = process.env.RESTAPI
@@ -20,6 +20,7 @@ const BITBOX = require("slp-sdk")
 
 const config = {}
 
+// Use BITBOX and the bitcoin.com infrastructure.
 if (RESTAPI === "bitcoin.com") {
   config.BCHLIB = BITBOX
   config.MAINNET_REST = `https://rest.bitcoin.com/v2/`
@@ -27,13 +28,25 @@ if (RESTAPI === "bitcoin.com") {
   config.RESTAPI = "bitcoin.com"
 }
 
+// Use bch-js but use it with the bitcoin.com infrastructure.
+if (RESTAPI === "rest.bitcoin.com") {
+  config.BCHLIB = BCHJS.BitboxShim()
+  config.MAINNET_REST = `https://rest.bitcoin.com/v2/`
+  config.TESTNET_REST = `https://trest.bitcoin.com/v2/`
+  config.RESTAPI = "rest.bitcoin.com"
+}
+
+// Use bch-js with local infrastructure.
 if (RESTAPI === "local") {
   config.BCHLIB = BCHJS.BitboxShim()
-  config.MAINNET_REST = `http://192.168.0.36:12400/v3/`
-  config.TESTNET_REST = `http://192.168.0.38:13400/v3/`
+  // config.MAINNET_REST = `http://192.168.0.36:12400/v3/`
+  // config.TESTNET_REST = `http://192.168.0.38:13400/v3/`
+  config.MAINNET_REST = `http://127.0.0.1:3000/v3/`
+  config.TESTNET_REST = `http://decatur.hopto.org:13400/v3/`
   config.RESTAPI = "local"
 }
 
+// Use bch-js with decatur infrastructure.
 if (RESTAPI === "decatur") {
   config.BCHLIB = BCHJS.BitboxShim()
   config.MAINNET_REST = `http://decatur.hopto.org:12400/v3/`
