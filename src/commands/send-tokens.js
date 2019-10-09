b81fd86c940596432830ede7f7e88d1d6613dc90
Chris Troutner <chris.troutner@gmail.com>
2019-10-08 20:29:01 -0700

    Removing token hex output when sending tokens

--- src/commands/send-tokens.js
+++ src/commands/send-tokens.js
@@ -113,7 +113,7 @@ class SendTokens extends Command {
         walletInfo,
         tokenUtxos
       )
-      console.log(`hex: ${hex}`)
+      // console.log(`hex: ${hex}`)
 
       const txid = await appUtils.broadcastTx(hex)
       appUtils.displayTxid(txid, walletInfo.network)
